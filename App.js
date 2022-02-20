bd1bedfe3baca238f1a31682d581ed92fe7e0133
Mehmet Furkan Adiguzel <55146992+Mehmetadiguzel1@users.noreply.github.com>
2022-02-20 08:35:44 -0500

    First Commit, pages created

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainRoot from './app/pages/MainRoot';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={MainRoot}
+          options={{ title: 'MainRoot' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
