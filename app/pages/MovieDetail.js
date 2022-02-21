4dc40a915e507d66d5654b7ba593656010b112c1
Mehmet Furkan Adiguzel <55146992+Mehmetadiguzel1@users.noreply.github.com>
2022-02-20 21:04:28 -0500

    In MovieDetail vote average and Overview added

--- app/pages/MovieDetail.js
+++ app/pages/MovieDetail.js
@@ -1,11 +1,47 @@
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView } from 'react-native'
 import React from 'react'
 import Constants from 'expo-constants';
-
 export default function MovieDetail({ navigation, route }) {
+  const movieItem = route.params.item;
   return (
     <View style={styles.container}>
-      <Text>{route.params.item.title}</Text>
+      <ScrollView>
+        <Image style={styles.poster} resizeMode={"cover"} source={{uri: "https://image.tmdb.org/t/p/original/" + movieItem.backdrop_path}} />
+
+        <View style={{ 
+          flex: 1, 
+          backgroundColor:"#B0B0B0",
+          padding: 20,
+          }}>
+          <View style={{ 
+            flex:1, 
+            flexDirection: "row", 
+            justifyContent: "space-between",
+            alignItems: "center",
+            }}>
+
+            <View style={{ flexWrap: "wrap", flexDirection: "column"}}>
+              <Text>{movieItem.title}</Text>
+              <Text>{movieItem.title}</Text>
+            </View>
+            <View style={{ 
+              width:48, 
+              height:48, 
+              backgroundColor:"white", 
+              borderRadius:24,
+              justifyContent:"center",
+              alignItems:"center",
+              
+              }}>
+                <Text>{movieItem.vote_average}</Text>
+            </View>
+          </View>
+          <Text style={styles.header}>Overview</Text>
+          <Text>{movieItem.overview}</Text>
+          <Text style={styles.header}>Teaser & Trailers</Text>
+
+        </View>
+      </ScrollView>
     </View>
   )
 }
@@ -16,5 +52,13 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: Constants.statusBarHeight,
 
+    },
+    poster:{
+      height: 230,
+    },
+    header:{
+      fontSize: 26,
+      fontWeight:"bold",
+      marginTop: 10,
     }
 })
