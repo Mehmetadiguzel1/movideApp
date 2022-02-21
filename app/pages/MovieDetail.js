6cd07ace241b86ccda8eefc57fa7dcb2295ca19d
Mehmet Furkan Adiguzel <55146992+Mehmetadiguzel1@users.noreply.github.com>
2022-02-21 10:30:50 -0500

    Key warning fixed

--- app/pages/MovieDetail.js
+++ app/pages/MovieDetail.js
@@ -3,70 +3,100 @@ import React from 'react'
 import Constants from 'expo-constants';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import ChipGroup from '../components/ChipGroup';
+import TeaserTrailer from './../models/TeaserTrailer';
+
+export default class MovieDetail extends React.Component {
+  movieItem = null;
+  constructor(props){
+    super(props);
+    this.movieItem = props.route.params.item;
+  }
+
+  state = {
+    teeserTrailers: [],
+
+  };
 
 
-export default function MovieDetail({ navigation, route }) {
-  const movieItem = route.params.item;
-  var genres = "";
-  movieItem.genres.map((genre, index ) => {
-    genres += genre + (index < movieItem.genres.length -1 ? ", " : "");
-  });
-  return (
-    <View style={styles.container}>
-      <ScrollView>
-        
-        <TouchableWithoutFeedback onPress={() => navigation.pop()} >
+  componentDidMount(){
+    return fetch('https://api.themoviedb.org/3/movie/' + this.movieItem.id + '/videos?api_key=def30dcb6753a6abdcf8682057b1ed85')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      var items = [];
+      responseJson.results.map(movie => {
+        items.push(new TeaserTrailer({key: movie.key, name: movie.name, type: movie.type,}));
+      });
+      this.setState({teeserTrailers: items});
+    })
+    .catch((error) => console.error(error));
+  }
+
+  render() {
+    
+    return (
+      <View style={styles.container}>
+        <ScrollView>
           
-          <MaterialCommunityIcons 
-        style={{ position: "absolute", top: Constants.statusBarHeight +10, left: 20, zIndex:1, paddingRight: 20, paddingBottom:20}} name="chevron-left" size={24} color={"#fff"} />
-        </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => navigation.pop()} >
+            
+            <MaterialCommunityIcons 
+          style={{ position: "absolute", top: Constants.statusBarHeight +10, left: 20, zIndex:1, paddingRight: 20, paddingBottom:20}} name="chevron-left" size={24} color={"#fff"} />
+          </TouchableWithoutFeedback>
 
-        <Image style={styles.poster} resizeMode={"cover"} source={{uri: "https://image.tmdb.org/t/p/original/" + movieItem.backdrop_path}} />
+          <Image style={styles.poster} resizeMode={"cover"} source={{uri: "https://image.tmdb.org/t/p/original/" + this.movieItem.backdrop_path}} />
 
-        <View style={{ 
-          flex: 1, 
-          
-          padding: 20,
-          }}>
           <View style={{ 
-            flex:1, 
-            flexDirection: "row", 
-            justifyContent: "space-between",
-            alignItems: "center",
-            marginBottom: 10,
-
+            flex: 1, 
+            
+            padding: 20,
             }}>
-
-            <View style={{ flexWrap: "wrap", flexDirection: "column"}}>
-              <Text style={styles.title}>{movieItem.title}</Text>
-              <Text>{movieItem.release_date}</Text>
-            </View>
             <View style={{ 
-              width:48, 
-              height:48, 
-              backgroundColor:"white", 
-              borderRadius:24,
-              justifyContent:"center",
-              alignItems:"center",
-              
+              flex:1, 
+              flexDirection: "row", 
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginBottom: 10,
+
               }}>
-                <Text>{movieItem.vote_average}</Text>
-            </View>
-          </View>
 
-              <View style={{width: "100%"}}>
-                <ChipGroup datas={movieItem.genres} />
+              <View style={{ flexWrap: "wrap", flexDirection: "column"}}>
+                <Text style={styles.title}>{this.movieItem.title}</Text>
+                <Text>{this.movieItem.release_date}</Text>
               </View>
+              <View style={{ 
+                width:48, 
+                height:48, 
+                backgroundColor:"white", 
+                borderRadius:24,
+                justifyContent:"center",
+                alignItems:"center",
+                
+                }}>
+                  <Text>{this.movieItem.vote_average}</Text>
+              </View>
+            </View>
+
+                <View style={{width: "100%"}}>
+                  <ChipGroup datas={this.movieItem.genres} />
+                </View>
 
 
-          <Text style={styles.header}>Overview</Text>
-          <Text>{movieItem.overview}</Text>
-          <Text style={styles.header}>Teaser & Trailers</Text>
+            <Text style={styles.header}>Overview</Text>
+            <Text>{this.movieItem.overview}</Text>
+            <Text style={styles.header}>Teaser & Trailers</Text>
+            <View>
+              {
+                this.state.teeserTrailers.map(item => {
+                  return<Text key={item.key}>{item.name}</Text>;
+                })
+              }
+            </View>
 
-        </View>
-      </ScrollView>
-    </View>
-  )
+          </View>
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 
