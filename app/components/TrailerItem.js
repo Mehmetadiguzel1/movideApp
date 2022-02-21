2792d7722ef80deed2fcffc0e78142e9fb2bae6a
Mehmet Furkan Adiguzel <55146992+Mehmetadiguzel1@users.noreply.github.com>
2022-02-21 11:41:43 -0500

    Trailer play button added. Not: not functional

--- app/components/TrailerItem.js
+++ app/components/TrailerItem.js
@@ -6,10 +6,29 @@ export default function TrailerItem(props) {
     const deviceWidth = Dimensions.get('window').width;
     const posterWidth = (deviceWidth - 50) / 2;
     console.log(posterWidth / 2);
+    const leftPosition = (posterWidth - 24) / 4;
 
     return (
         <View style={{ marginRight: 5, }} >
-            <Image resizeMode={'cover'} style={{width: posterWidth, height: 100,}} source={{uri:'https://image.tmdb.org/t/p/original/' + props.poster}} />
+            <Image
+            style={{ 
+                position: 'absolute',
+                top: 20,
+                left: leftPosition, 
+                zIndex: 1,
+                width: 64,
+                height: 64,
+            
+            }} 
+            source={require('./../../assets/play.png')} />
+            <Image resizeMode={'cover'} 
+            style={{
+                width: posterWidth, 
+                height: 100,
+                borderRadius: 20,
+                marginBottom: 5,
+                }} 
+                source={{uri:'https://image.tmdb.org/t/p/original/' + props.poster}} />
             <Text style={{flexWrap: 'wrap', width: posterWidth}}>{props.data.name}</Text>
         </View>
     )
